9f91dff32c3d2808d7e27de7ccdbd35048b6621a
Denis Sokolov <57729207+denissokolow@users.noreply.github.com>
2020-05-29 00:33:29 +0300

    Update stream.js

--- stream.js
+++ stream.js
@@ -1,8 +1,9 @@
-
 const fs = require("fs");
 let buf = "";
 const rdStr = fs.createReadStream("1.txt", { highWaterMark: 1 });
+
 rdStr.on("data", data => {
   buf = buf + (+data + 1);
 });
+
 rdStr.on("end", () => fs.createWriteStream("1.txt").write(buf));
